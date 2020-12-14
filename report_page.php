6f814087860027fde0f48872d7e851b9e181dc29
Deepti Pandey <deeptipandey111@gmail.com>
2020-12-14 11:21:59 -0500

    adding php file

---
+++ report_page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Feedback</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- CSS here -->
+	<link rel="stylesheet" href="css/exit.css">
+  <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<div class="container">
+
+
+
+  <div class="sub-container">
+    <h1>Thanks for using UMMA connect!!</h1><br><br>
+    <?php
+    echo "Thank you for making UMMA connect safer with your report!";
+    ?>
+  </div>
+  <br><br>
+
+  <br><br>
+
+
+</div>
+
+
+
+</body>
+
+<!-- javascript here -->
+<script type="text/javascript" src="js/exit.js"></script>
+
+</html>
